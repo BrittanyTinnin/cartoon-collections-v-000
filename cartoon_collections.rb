d88ec4703b26ef542681806e70460d310870c0a5
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-06 13:16:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find { | | }
+  strings.find { |food| puts food }
 end
